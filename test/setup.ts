5375d07e2a10364ca1bfc48fa71845f689c8b333
Bruno Mendes Pimenta <bruno.mendes51@hotmail.com>
2025-01-11 16:27:38 -0300

    chore: configura jest para subir um mysql para os testes end to end

---
+++ test/setup.ts
@@ -0,0 +1,38 @@
+import * as Docker from 'dockerode';
+
+export default async function() {
+
+    return new Promise(async (resolve, reject) => {
+        const docker = new Docker({socketPath: '/var/run/docker.sock'});
+
+        console.log('\nCriando container Docker MySQL 8.0');
+
+        const container = await docker.createContainer({
+            name: 'orcamentos_tests',
+            Image: 'mysql:8.0',
+            Env: [
+            'MYSQL_ROOT_PASSWORD=root',
+            'MYSQL_DATABASE=orcamentos',
+            ],
+            ExposedPorts: {
+            '3306/tcp': {},
+            },
+            HostConfig: {
+            PortBindings: {
+                '3306/tcp': [
+                {
+                    HostPort: '3306',
+                },
+                ],
+            },
+            },
+        });
+
+        await container.start();
+
+        setTimeout(() => {
+            console.log('Container MySQL 8.0 iniciado com sucesso');
+            resolve(true)
+        }, 20000);
+    });
+}
---
+++ test/tearDown.ts
@@ -0,0 +1,15 @@
+import * as Docker from "dockerode";
+
+
+export default async function() {
+  const docker = new Docker({socketPath: '/var/run/docker.sock'});
+  const container = docker.getContainer('orcamentos_tests');
+
+  console.log('Parando container MySQL 8.0')
+  await container.stop();
+
+  console.log('Removendo container MySQL 8.0')
+  await container.remove();
+
+  console.log('Container MySQL 8.0 removido com sucesso');
+}
