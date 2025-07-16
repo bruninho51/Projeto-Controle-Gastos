ada75f8d8cfd269a6fdd490b5eac3001c48f8530
Bruno Mendes Pimenta <bruno.mendes51@hotmail.com>
2025-07-16 20:27:26 -0300

    fix: abilita patch

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: "*",
-    methods: "GET,POST,PUT,DELETE,OPTIONS",
+    methods: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
     allowedHeaders: "Content-Type, Authorization",
   });
 
