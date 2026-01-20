3598062b4088ae9eaea9a4cddcff764e9083904f
Bruno Mendes Pimenta <bruno.pimenta@finnet.com.br>
2026-01-20 16:47:00 -0300

    feat: atualiza prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,11 @@
-import { defineConfig, env } from 'prisma/config';
+import { defineConfig } from 'prisma/config';
 import 'dotenv/config';
+
+const env = (key: string, fallback: string) => process.env[key] ?? fallback
 
 export default defineConfig({
   schema: 'prisma/schema.prisma',
   datasource: {
-    url: `mysql://${env('DB_USER')}:${encodeURIComponent(env('DB_PASSWORD'))}@${env('DB_HOST')}:${env('DB_PORT')}/${env('DB_NAME')}`,
+    url: `mysql://${env('DB_USER', 'user')}:${encodeURIComponent(env('DB_PASSWORD', 'pass'))}@${env('DB_HOST', 'localhost')}:${env('DB_PORT', '3306')}/${env('DB_NAME', 'dummy')}`,
   },
-})
+});
